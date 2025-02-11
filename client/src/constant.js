c0a8a63e573c944c6d0db5d85b04d7758ede86f2
Siva-S92 <sivagraphics4fashion@gmail.com>
2025-02-11 17:04:35 +0530

    change the server url

--- client/src/constant.js
+++ client/src/constant.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = 'http://localhost:5000'; // Change to your backend URL
+// export const API_BASE_URL = 'http://localhost:5000'; // Change to your backend URL
+export const API_BASE_URL = 'https://360degree-info-backend.vercel.app'; // Change to your backend URL
